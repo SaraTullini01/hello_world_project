9cc191dee6d9d9616b37cd091ffc4bf4d6c97499
SaraTullini <sara.tullini@edu.unife.it>
2023-12-21 13:13:01 +0100

    versione 3.0

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! v:2.0'
+    return 'Hello World! v:3.0'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
